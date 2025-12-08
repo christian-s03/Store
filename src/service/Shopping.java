ba110e1728b2d5f90387fd52fc6f38957ca81c41
christian-s03 <suskikrystian8@gmail.com>
2025-12-08 13:04:36 +0100

    Completed changes: made shorter methods.

--- src/service/Shopping.java
+++ src/service/Shopping.java
@@ -16,8 +16,7 @@ public class Shopping implements CommandLine {
     private PcComponents pcComponents;
     private List<Product> products;
 
-    public Shopping(ProductManager productManager, Cart cart, OrderProcessor orderProcessor,
-                    Scanner scanner, PcComponents pcComponents) {
+    public Shopping(ProductManager productManager, Cart cart, OrderProcessor orderProcessor, Scanner scanner, PcComponents pcComponents) {
         this.productManager = productManager;
         this.cart = cart;
         this.orderProcessor = orderProcessor;
@@ -59,32 +58,41 @@ public class Shopping implements CommandLine {
     }
 
     public void addProductToCart() {
-
         System.out.println("Please enter the product ID (UUID) that you want to add to cart: ");
-        UUID productId = UUID.fromString(scanner.nextLine());
+
+        UUID productId;
+        try {
+            productId = UUID.fromString(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid UUID format");
+            return;
+        }
 
         Optional<Product> optionalProduct = productManager.findById(productId);
 
-        if (optionalProduct.isPresent()) {
-            Product foundProduct = optionalProduct.get();
-
-            System.out.println("How many products you want to add to cart: ");
-            int quantity = scanner.nextInt();
-            scanner.nextLine();
-
-            if (foundProduct.getQuantityAvailable() < quantity) {
-                System.out.println("There is not enough products to add to cart");
-                return;
-            }
-            for (int i = 0; i < quantity; i++) {
-                products.add(foundProduct);
-            }
-            productManager.removeProduct(foundProduct.getId(), quantity);
-            System.out.println("Product added to cart");
-        } else {
+        if (optionalProduct.isEmpty()) {
             System.out.println("Product ID not found");
+            return;
         }
+
+        Product foundProduct = optionalProduct.get();
+
+        System.out.println("How many products do you want to add to cart: ");
+        int quantity = scanner.nextInt();
+        scanner.nextLine();
+
+        if (foundProduct.getQuantityAvailable() < quantity) {
+            System.out.println("There is not enough products to add to cart");
+            return;
+        }
+
+        products.addAll(Collections.nCopies(quantity, foundProduct));
+
+        productManager.removeProduct(foundProduct.getId(), quantity);
+
+        System.out.println("Product added to cart");
     }
+
 
     @Override
     public void removeProductFromCart() {
@@ -118,125 +126,124 @@ public class Shopping implements CommandLine {
 
     public void setUpPc() throws NotAvailableException {
         int command;
+
         do {
             System.out.println("Set up your new Pc. Starting price is 2000.00 zł + components");
-            Computer computer = new Computer(UUID.randomUUID(), "Pc", new BigDecimal(0.00), 10,
-                    null, null, null, null);
+
+            Computer computer = createBaseComputer();
             BigDecimal totalCost = BigDecimal.valueOf(2000.00);
 
-            System.out.println("Pick Processor: ");
-            displayComponents(pcComponents.getProcessors());
-            int processorChoice = scanner.nextInt();
-            PcComponents.Component chosenProcessor = pcComponents.getProcessors().get(processorChoice - 1);
-            System.out.println("You picked: " + chosenProcessor.getProductName());
-            computer.setProcessor(chosenProcessor.getProductName());
-            totalCost = totalCost.add(chosenProcessor.getPrice());
+            totalCost = totalCost.add(chooseProcessor(computer));
+            totalCost = totalCost.add(chooseRam(computer));
+            totalCost = totalCost.add(chooseSsd(computer));
+            totalCost = totalCost.add(chooseCharger(computer));
 
-            System.out.println("Pick Ram: ");
-            displayComponents(pcComponents.getRam());
-            int ramChoice = scanner.nextInt();
-            PcComponents.Component chosenRam = pcComponents.getRam().get(ramChoice - 1);
-            computer.setRam(chosenRam.getProductName());
-            totalCost = totalCost.add(chosenRam.getPrice());
+            askToAddPcToCart(computer, totalCost);
 
-            System.out.println("Pick Ssd: ");
-            displayComponents(pcComponents.getSsd());
-            int ssdChoice = scanner.nextInt();
-            PcComponents.Component chosenSsd = pcComponents.getSsd().get(ssdChoice - 1);
-            computer.setSsd(chosenSsd.getProductName());
-            totalCost = totalCost.add(chosenSsd.getPrice());
+            command = waitForReturnToMenu();
 
-            System.out.println("Pick charger: ");
-            displayComponents(pcComponents.getChargers());
-            int chargerChoice = scanner.nextInt();
-            PcComponents.Component chosenCharger = pcComponents.getChargers().get(chargerChoice - 1);
-            computer.setCharger(chosenCharger.getProductName());
-            totalCost = totalCost.add(chosenCharger.getPrice());
-
-            System.out.println("Would you like add your PC to cart? ( YES / NO )");
-            String addToCartChoice = scanner.next();
-            if (addToCartChoice.equalsIgnoreCase("YES")) {
-                productManager.addProduct(computer);
-                computer.setPrice(totalCost);
-                cart.addProductToCart(computer, 1);
-                System.out.println("Total PC price: " + totalCost + " zł");
-            } else {
-                System.out.println("PC not added to cart");
-            }
-            System.out.println("Click 0 to go back to menu");
-            command = scanner.nextInt();
-            scanner.nextLine();
         } while (command != 0);
+    }
+
+    private Computer createBaseComputer() {
+        return new Computer(
+                UUID.randomUUID(),
+                "Pc",
+                BigDecimal.ZERO,
+                10,
+                null, null, null, null
+        );
+    }
+
+    private BigDecimal chooseProcessor(Computer computer) {
+        System.out.println("Pick Processor: ");
+        displayComponents(pcComponents.getProcessors());
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        PcComponents.Component c = pcComponents.getProcessors().get(choice - 1);
+
+        System.out.println("You picked: " + c.getProductName());
+        computer.setProcessor(c.getProductName());
+
+        return c.getPrice();
+    }
+
+    private BigDecimal chooseRam(Computer computer) {
+        System.out.println("Pick Ram: ");
+        displayComponents(pcComponents.getRam());
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        PcComponents.Component c = pcComponents.getRam().get(choice - 1);
+
+        computer.setRam(c.getProductName());
+        return c.getPrice();
+    }
+
+    private BigDecimal chooseSsd(Computer computer) {
+        System.out.println("Pick Ssd: ");
+        displayComponents(pcComponents.getSsd());
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        PcComponents.Component c = pcComponents.getSsd().get(choice - 1);
+
+        computer.setSsd(c.getProductName());
+        return c.getPrice();
+    }
+
+    private BigDecimal chooseCharger(Computer computer) {
+        System.out.println("Pick charger: ");
+        displayComponents(pcComponents.getChargers());
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        PcComponents.Component c = pcComponents.getChargers().get(choice - 1);
+
+        computer.setCharger(c.getProductName());
+        return c.getPrice();
+    }
+
+    private void askToAddPcToCart(Computer computer, BigDecimal totalCost) throws NotAvailableException {
+        System.out.println("Would you like add your PC to cart? ( YES / NO )");
+        String choice = scanner.nextLine();
+        if (choice.equalsIgnoreCase("YES")) {
+            productManager.addProduct(computer);
+            computer.setPrice(totalCost);
+            cart.addProductToCart(computer, 1);
+
+            System.out.println("Total PC price: " + totalCost + " zł");
+        } else {
+            System.out.println("PC not added to cart");
+        }
+    }
+
+    private int waitForReturnToMenu() {
+        System.out.println("Click 0 to go back to menu");
+        while (true) {
+            try {
+                int command = Integer.parseInt(scanner.nextLine().trim());
+                return command;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number");
+            }
+        }
     }
 
     @Override
     public void setUpPhone() throws NotAvailableException {
-        int command = 0;
-        Smartphone smartphone = new Smartphone(UUID.randomUUID(), "Phone", BigDecimal.ZERO,
-                1, null, 0);
+        Smartphone smartphone = createBaseSmartphone();
         BigDecimal totalCost = BigDecimal.valueOf(1000.00);
+
         System.out.println("Set up your phone. Starting price is " + totalCost);
-        System.out.println("Pick color of your phone: ");
-        Smartphone.displayAvailableColors();
-        int colorChoice = scanner.nextInt();
-        scanner.nextLine();
-        Smartphone.Color color = Smartphone.Color.values()[colorChoice - 1];
-        smartphone.setColor(String.valueOf(color));
-        System.out.println("Pick battery: ");
-        System.out.println("1 - 1500 mAh - 29.99 zł");
-        System.out.println("2 - 2500 mAh - 49.99 zł");
-        System.out.println("3 - 3500 mAh - 79.99 zł");
-        int batteryChoice = scanner.nextInt();
-        switch (batteryChoice) {
-            case 1 -> {
-                smartphone.setBatteryCapacity(1500);
-                totalCost = totalCost.add(BigDecimal.valueOf(29.99));
-            }
-            case 2 -> {
-                smartphone.setBatteryCapacity(2500);
-                totalCost = totalCost.add(BigDecimal.valueOf(49.99));
-            }
-            case 3 -> {
-                smartphone.setBatteryCapacity(3500);
-                totalCost = totalCost.add(BigDecimal.valueOf(79.99));
-            }
-            default -> {
-                System.out.println("Invalid battery choice");
-                return;
-            }
-        }
-        scanner.nextLine();
-        System.out.println("Pick accessories to your phone: ");
-        while (true) {
-            System.out.println("1 - Powerbank - 399.99 zł");
-            System.out.println("2 - Case - 29.99 zł");
-            System.out.println("3 - Charger - 99.99 zł");
-            System.out.println("0 - No accessories");
-            int accessoriesChoice = scanner.nextInt();
-            scanner.nextLine();
-            if (accessoriesChoice == 0) {
-                break;
-            }
-            switch (accessoriesChoice) {
-                case 1 -> totalCost = totalCost.add(BigDecimal.valueOf(399.99));
-                case 2 -> totalCost = totalCost.add(BigDecimal.valueOf(29.99));
-                case 3 -> totalCost = totalCost.add(BigDecimal.valueOf(99.99));
-                default -> System.out.println("Invalid accessories choice");
-            }
-        }
-        System.out.println("Would you like add your Smartphone to cart? ( YES / NO )");
-        String addToCartChoice = scanner.nextLine();
-        if (addToCartChoice.equalsIgnoreCase("YES")) {
-            productManager.addProduct(smartphone);
-            smartphone.setPrice(totalCost);
-            cart.addProductToCart(smartphone, 1);
-            System.out.println("Total smartphone price: " + totalCost + " zł");
-        } else {
-            System.out.println("Smartphone not added to cart");
-        }
-        System.out.println("Click 0 to go back to menu");
-        command = scanner.nextInt();
-        scanner.nextLine();
+
+        chooseColor(smartphone);
+        totalCost = totalCost.add(chooseBattery(smartphone));
+        totalCost = totalCost.add(chooseAccessories());
+
+        askToAddToCart(smartphone, totalCost);
+        waitForReturnToMenu();
     }
 
     @Override
@@ -250,6 +257,90 @@ public class Shopping implements CommandLine {
                 break;
             }
             System.out.println("Invalid input. Try again");
+        }
+    }
+
+    private Smartphone createBaseSmartphone() {
+        return new Smartphone(UUID.randomUUID(), "Phone", BigDecimal.ZERO, 1, null, 0);
+    }
+
+    private void chooseColor(Smartphone smartphone) {
+        System.out.println("Pick color of your phone: ");
+        Smartphone.displayAvailableColors();
+
+        int colorChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        Smartphone.Color color = Smartphone.Color.values()[colorChoice - 1];
+        smartphone.setColor(color.name());
+    }
+
+    private BigDecimal chooseBattery(Smartphone smartphone) {
+        System.out.println("Pick battery: ");
+        System.out.println("1 - 1500 mAh - 29.99 zł");
+        System.out.println("2 - 2500 mAh - 49.99 zł");
+        System.out.println("3 - 3500 mAh - 79.99 zł");
+
+        int batteryChoice = scanner.nextInt();
+
+        return switch (batteryChoice) {
+            case 1 -> {
+                smartphone.setBatteryCapacity(1500);
+                yield BigDecimal.valueOf(29.99);
+            }
+            case 2 -> {
+                smartphone.setBatteryCapacity(2500);
+                yield BigDecimal.valueOf(49.99);
+            }
+            case 3 -> {
+                smartphone.setBatteryCapacity(3500);
+                yield BigDecimal.valueOf(79.99);
+            }
+            default -> {
+                System.out.println("Invalid battery choice");
+                yield BigDecimal.ZERO;
+            }
+        };
+    }
+
+    private BigDecimal chooseAccessories() {
+        BigDecimal cost = BigDecimal.ZERO;
+
+        System.out.println("Pick accessories to your phone: ");
+
+        while (true) {
+            System.out.println("1 - Powerbank - 399.99 zł");
+            System.out.println("2 - Case - 29.99 zł");
+            System.out.println("3 - Charger - 99.99 zł");
+            System.out.println("0 - No accessories");
+
+            int accessoriesChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (accessoriesChoice) {
+                case 0 -> {
+                    return cost;
+                }
+                case 1 -> cost = cost.add(BigDecimal.valueOf(399.99));
+                case 2 -> cost = cost.add(BigDecimal.valueOf(29.99));
+                case 3 -> cost = cost.add(BigDecimal.valueOf(99.99));
+                default -> System.out.println("Invalid accessories choice");
+            }
+        }
+    }
+
+    private void askToAddToCart(Smartphone smartphone, BigDecimal totalCost) throws NotAvailableException {
+        System.out.println("Would you like to add your Smartphone to cart? ( YES / NO )");
+        scanner.nextLine();
+        String addToCartChoice = scanner.nextLine();
+
+        if (addToCartChoice.equalsIgnoreCase("YES")) {
+            productManager.addProduct(smartphone);
+            smartphone.setPrice(totalCost);
+            cart.addProductToCart(smartphone, 1);
+            System.out.println("Total smartphone price: " + totalCost + " zł");
+        } else {
+            System.out.println("Smartphone not added to cart");
         }
     }
 
